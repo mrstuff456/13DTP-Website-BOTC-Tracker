523fe616d5f7ed3ad129a8736704cc77c0e5bb22
mrstuff456 <115061061+mrstuff456@users.noreply.github.com>
2026-03-27 11:21:50 +1300

    flasklogin and seat functionalilty
    added base bits to run flasklogin
    added "game.html" that can add/remove seats and arrange them in a circle

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, session, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, ForeignKey, Table, Column, select
+from flask_login import LoginManager
 
 
 # init app
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+app.secret_key = 'a091e1b6b9a25a1e60fab1e0b57aae48febd2a1ffeb250a0dc7169d064f67586d'
 
 
 # init db
@@ -13,12 +17,20 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 # routes
 @app.route('/')
 def home():
     return render_template('home.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 
 if __name__ == "__main__":
